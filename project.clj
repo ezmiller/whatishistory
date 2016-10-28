c61317221e9e8fe6c11a996b43d83171ed70ef26
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2016-10-28 15:17:33 +0300

    Update project details

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject whatishistory "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject whatishistory "0.9"
+  :description "A site for collecting definitions of history."
+  :url "http://whatishistory.net"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
